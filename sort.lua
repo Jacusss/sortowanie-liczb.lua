f196cf3cf213421dae40abeeda69cc3d78018add
Jacusss <83212643+Jacusss@users.noreply.github.com>
2021-05-04 17:35:35 +0200

    Update sort.lua

--- sort.lua
+++ sort.lua
@@ -1,37 +1,29 @@
 print("podaj ilosc liczb :")
-  b = io.read( )             --podawanie ilosci liczb
+b = io.read( )             --podawanie ilosci liczb
+
 local n={b}
 
-a=0
-for i=1,b,1 do
-print("podaj liczbe :")
-  n[a+i] = io.read( )
+for i=0,b-1,1 do
+  print("podaj liczbe :")
+  n[i] = io.read( )
 end  -- wpisywanie do tabeli
 
 print("tabela nieposortowana :")
-for i=1,b,1 do
-print(n[0+i])  --wywolanie tabeli
+for i=0,b-1,1 do
+  print(n[i])  --wywolanie tabeli
 end
 
-local function sort(t, nsort) -- sortowanie
-  local count = #n
-    for j = 1, count -1 do
-      for i = 1, count - 1 do
-       local pre = n[i]
-       local nex = n[i+1]
-          if nsort and pre> nex then
-        n[i] = nex
-        n[i + 1] = pre
-      end 
+function sort(n)
+  for i=0,b-1,1 do
+    for j=0, j <b-1 do
+      if n[j] > n[j+1]     then
+        temp = n[j]
+        n[j] = n[j+1]
+        n[j+1] = temp
+      end
     end
   end
 end
 
-local function tabela(t)
-  for k, v in pairs(t) do   --funkcja wypisywania tabeli rosnaco
-    print(v)
-  end
-end
-print("posortowane rosnaco:")  
-sort(n, true)
-tabela(n)           -- wywołanie funkcji
+print(sort(n))
+
