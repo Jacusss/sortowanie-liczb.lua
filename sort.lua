6242fc011a3028f28c192500f9b190903e33baf7
Jacusss <83212643+Jacusss@users.noreply.github.com>
2021-05-04 18:24:14 +0200

    Update sort.lua

--- sort.lua
+++ sort.lua
@@ -2,15 +2,14 @@ print("podaj ilosc liczb :")
   b = io.read( )                     --podawanie ilosci liczb
 local n={b}
 
-a=0
 for i=1,b,1 do
 print("podaj liczbe :")
-  n[a+i] = io.read( )               -- wpisywanie do tabeli
+  n[i] = io.read( )               -- wpisywanie do tabeli
 end 
 
 print("tabela nieposortowana :")
 for i=1,b,1 do
-print(n[0+i])                       --wywolanie tabeli nieposortowana  
+print(n[i])                       --wywolanie tabeli nieposortowana  
 end
 io.write("tabela posortowana :\n")
 table.sort(n)                       --sortowanie
